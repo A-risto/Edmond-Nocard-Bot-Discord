a3fe0cd8682a15a549e1dc7e6766ff4e0c5ff9cb
A-risto <100279750+A-risto@users.noreply.github.com>
2022-02-28 11:43:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -329,6 +329,7 @@ async def on_member_remove(member):
 @bot.command()
 async def repete(ctx, *texte):
     await ctx.send(" ".join(texte))
+    await ctx.message.delete()
 
 
 @bot.event
@@ -446,6 +447,24 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.CheckFailure):
         await ctx.send("Oups vous ne pouvez iutilisez cette commande.")
 
-        
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.guild.id == 888163341174992966:
+        channel = after.channel
+        try:
+            if before.channel.members == [] and not before.channel.id == 947804187926863902:
+                if before.channel.category_id == 888163341174992968:
+                    await before.channel.delete()
+        except:
+            pass
+
+        if channel.id == 947804187926863902:
+            guild = after.channel.guild
+            private_channels = discord.utils.get(guild.categories, id=888163341174992968)
+            voice_channel = await guild.create_voice_channel(member.name, overwrites=None, category=private_channels)
+            await member.move_to(voice_channel)
+            await voice_channel.set_permissions(member, connect=True, speak=True, move_members=True, manage_channels=True, view_channel=True)
+
+
 token = 'TOKEN'
 bot.run(token)
