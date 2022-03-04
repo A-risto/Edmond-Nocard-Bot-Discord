7e5eb39d3a1cfd3a226a134cda5f102267f53d66
A-risto <100279750+A-risto@users.noreply.github.com>
2022-03-04 09:49:11 +0100

    Update and rename main.py to EdmondNocardBot.py

--- EdmondNocardBot.py
+++ EdmondNocardBot.py
@@ -443,25 +443,5 @@ async def on_command_error(ctx, error):
         await ctx.send("Oups vous ne pouvez iutilisez cette commande.")
 
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-    if member.guild.id == 888163341174992966:
-        channel = after.channel
-        try:
-            if before.channel.members == [] and not before.channel.id == 947804187926863902 and not before.channel.id == 947832541660983306:
-                if before.channel.category_id == 888163341174992968:
-                    await before.channel.delete()
-        except:
-            pass
-
-        if channel.id == 947804187926863902:
-            guild = after.channel.guild
-            private_channels = discord.utils.get(guild.categories, id=888163341174992968)
-            voice_channel = await guild.create_voice_channel("Salon de "+member.name, overwrites=None, category=private_channels)
-            await member.move_to(voice_channel)
-            await voice_channel.set_permissions(member, connect=True, speak=True, move_members=True, manage_channels=True, view_channel=True)
-
-
 token = 'TOKEN'
 bot.run(token)
-
